0aafd76525ad574ed491b21c594152dd978fee9c
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-26 22:40:11 +0200

    build: Added Retrofit Converter-Scalars dependency

--- preproduction/aemet-open-data-tests/build.gradle.kts
+++ preproduction/aemet-open-data-tests/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-scalars:3.0.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0-0.6.x-compat")
 }
