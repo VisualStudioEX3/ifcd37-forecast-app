52c047406946d6603f8f95d9e1f455295c6c8598
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-26 03:46:47 +0200

    refactor: Renamed AemetSnowData to AemetSnowLevelData

--- preproduction/aemet-open-data-tests/src/main/kotlin/aemet/models/data/AemetSnowLevelData.kt
+++ preproduction/aemet-open-data-tests/src/main/kotlin/aemet/models/data/AemetSnowLevelData.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AemetSnowData(
+data class AemetSnowLevelData(
     val value: Int,
     @SerialName("periodo") val period: String
 )
