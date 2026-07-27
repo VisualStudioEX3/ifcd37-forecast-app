79e2064b3ebaea79124760e72b5d8f3b5ce6b4d4
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-28 01:36:04 +0200

    feat: Implemented the complete two forecast requests to test
    Implemented daily and hourly requests with the second request to get the
    forecast data for each one.

--- preproduction/aemet-open-data-tests/src/main/kotlin/Main.kt
+++ preproduction/aemet-open-data-tests/src/main/kotlin/Main.kt
@@ -1,18 +1,56 @@
 package org.example
 
-import org.example.aemet.models.responses.AemetOpenDataResponse
+import org.example.aemet.models.responses.AemetDailyForecastByCityResponse
+import org.example.aemet.models.responses.AemetHourlyForecastByCityResponse
 import org.example.aemet.service.AemetApi
 import retrofit2.HttpException
 
 suspend fun main() {
-    val response: AemetOpenDataResponse = try {
-        AemetApi.endpoints.getDailyForecatsByCity(
-            Secrets.getSecret("aemet_opendata_api_key"),
-            "28058"
-        )
-    } catch (e: HttpException) {
-        AemetApi.getErrorResponseBody(e)
-    }
+    val apiKey: String = Secrets.getSecret("aemet_opendata_api_key")
+    val cityCode = "28058"
 
-    println(response)
+    requestDailyForecast(apiKey, cityCode)
+    requestHourlyForecast(apiKey, cityCode)
+}
+
+suspend fun requestDailyForecast(apiKey: String, cityCode: String) {
+    try {
+        AemetApi.endpoints.getDailyForecatsByCity(apiKey, cityCode).also {
+            try {
+                println("Daily forecast:")
+                println(it)
+
+                val forecast: AemetDailyForecastByCityResponse =
+                    AemetApi.endpoints.getDailyForescastData(apiKey, it.getDataId())
+                        .first()
+
+                println(forecast)
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    } catch (e: HttpException) {
+        println(AemetApi.getErrorResponseBody(e))
+    }
+}
+
+suspend fun requestHourlyForecast(apiKey: String, cityCode: String) {
+    try {
+        AemetApi.endpoints.getHourlyForecatsByCity(apiKey, cityCode).also {
+            try {
+                println("Hourly forecast:")
+                println(it)
+
+                val forecast: AemetHourlyForecastByCityResponse =
+                    AemetApi.endpoints.getHourlyForescastData(apiKey, it.getDataId())
+                        .first()
+
+                println(forecast)
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    } catch (e: HttpException) {
+        println(AemetApi.getErrorResponseBody(e))
+    }
 }
