ea99d09430b18c9ca9d99b915e9ee7f59f08b9a8
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-07-27 02:02:05 +0200

    feat: Implemented minimal functional AEMET OpenData API endpoints service
    And also basic test against one of the available endpoints.

--- preproduction/aemet-open-data-tests/src/main/kotlin/Main.kt
+++ preproduction/aemet-open-data-tests/src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.example.aemet.models.responses.AemetOpenDataResponse
+import org.example.aemet.service.AemetApi
+import retrofit2.HttpException
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+suspend fun main() {
+    val response: AemetOpenDataResponse = try {
+        AemetApi.endpoints.getDailyForecatsByCity(
+            Secrets.getSecret("aemet_opendata_api_key"),
+            "28058"
+        )
+    } catch (e: HttpException) {
+        AemetApi.getErrorResponseBody(e)
     }
+
+    println(response)
 }
