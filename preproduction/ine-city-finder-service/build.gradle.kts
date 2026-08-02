a080416833848f064b366e86924112d813520572
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-02 15:42:35 +0200

    build: Solved Slf4j annoying log warnings messages of not provider found
    Kotlin Dataframe used Slf4j logger under the hood. Seems as bug when
    program initializes that failed to find a valid provider and shows
    annoying log warnings messages on log output.
    
    Forcing to add Slf4j-NOP provider solve these warnings but still
    initializes Slf4j as logger (because Kotlin Dataframe used Kotlin Loggin
    internally).
    
    This bug is harmless, only add noise to log output, not compromise the
    Kotlin Dataframe right execution.

--- preproduction/ine-city-finder-service/build.gradle.kts
+++ preproduction/ine-city-finder-service/build.gradle.kts
@@ -12,7 +12,18 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:dataframe:1.0.0-rc01")
+    implementation("org.jetbrains.kotlinx:dataframe:1.0.0-rc01") {
+        /*
+         * BUG: Kotlin Dataframe used Slf4j logger under the hood. Seems as bug when program initializes that failed to
+         * find a valid provider and shows annoying log warnings messages on log output.
+         *
+         * Forcing to add Slf4j-NOP provider solve these warnings but still initializes Slf4j as logger (because Kotlin
+         * Dataframe used Kotlin Loggin internally).
+         *
+         * This bug is harmless, only add noise to log output, not compromise the Kotlin Dataframe right execution.
+         */
+        implementation("org.slf4j:slf4j-nop:2.0.18")
+    }
     implementation("org.jetbrains.kotlinx:dataframe-core:1.0.0-rc01")
     implementation("org.jetbrains.kotlinx:dataframe-excel:1.0.0-rc01") {
         exclude("org.jetbrains.kotlinx", "dataframe-json")
