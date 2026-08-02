28b0efac4a86a4c1c4dbd17bbf1d0d1edd9aaa87
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-02 14:53:05 +0200

    feat: Improved Main.kt terminal program to ease test the service

--- preproduction/ine-city-finder-service/src/main/kotlin/Main.kt
+++ preproduction/ine-city-finder-service/src/main/kotlin/Main.kt
@@ -1,13 +1,55 @@
 package org.example
 
+import org.example.ine.models.IneCityData
 import org.example.ine.services.IneService
 
-suspend fun main() {
-    print("Enter a name of a city: ")
-    val name: String = readln()
-    val result = IneService().findCitiesByName(name)
+val service = IneService()
 
-    result.forEach {
-        println(it)
+suspend fun main() {
+    var select: String
+
+    println("INE service simple terminal program.")
+
+    do {
+        print(
+            "\nSelect option:\n" +
+                    "1: Find cities by name.\n" +
+                    "2: Get state name by code.\n" +
+                    "0: Exit.\n" +
+                    ">"
+        ); select = readln()
+
+        when (select) {
+            "1" -> findCitiesByName()
+            "2" -> getStateNameByCode()
+            else -> println("Wrong selection. Try again.")
+        }
+    } while (select != "0")
+}
+
+suspend fun findCitiesByName() {
+    try {
+        print("Enter a name of a city: ")
+        val name: String = readln()
+        val result: List<IneCityData> = service.findCitiesByName(name)
+
+        println("Found ${result.size} matches:")
+        result.forEach {
+            println(it)
+        }
+    } catch (e: Exception) {
+        println(e.message)
+    }
+}
+
+suspend fun getStateNameByCode() {
+    try {
+        print("Enter a code of a state: ")
+        val code: String = readln()
+        val result: String = service.getStateNameByCode(code)
+
+        println("State name: $result")
+    } catch (e: Exception) {
+        println(e.message)
     }
 }
