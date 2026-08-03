5c2088e4bbb000ff3f95333e0ddea180d09529bd
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-03 20:36:11 +0200

    feat: Implemented INE province repository service

---
+++ project/IFCD37WeatherForecast/app/adapter/src/main/java/com/visualstudioex3/apdater/inedb/repositories/IneProvinceRepositoryImplementation.kt
@@ -0,0 +1,37 @@
+package com.visualstudioex3.apdater.inedb.repositories
+
+import com.visualstudioex3.apdater.inedb.DataFrameExcelImporter
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
+import org.jetbrains.kotlinx.dataframe.api.firstOrNull
+
+class IneProvinceRepositoryImplementation(
+    excelImporter: DataFrameExcelImporter
+): IneProvinceRepository {
+    /** @suppress  */
+    @Suppress("PropertyName", "unused")
+    @DataSchema
+    interface DataRowSchema {
+        val CODIGO: String
+        val LITERAL: String
+    }
+
+    private val provinces: DataFrame<DataRowSchema> = excelImporter.importExcelAsDataFrame(
+        resourceName = "codprov.xls",
+        sheetName = "Hoja1",
+        skipRows = 1,
+        stringColumns = "A"
+    )
+
+    override fun getName(
+        code: String
+    ): String = require(code.isNotBlank()) {
+            "The code can not be empty or blank string."
+        }.run {
+            provinces
+                .firstOrNull {
+                    CODIGO == code
+                }?.LITERAL
+                ?: throw NoSuchElementException("Province by code '$code' not found.")
+        }
+}
