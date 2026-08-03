393408ec9804811ec9164fb6a9ae26846418b7d6
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-03 14:43:16 +0200

    feat: Setup Hilt initialization on main app

---
+++ project/IFCD37WeatherForecast/app/src/main/java/com/visualstudioex3/ifcd37weatherforecast/IFCD37WeatherForecastApplication.kt
@@ -0,0 +1,10 @@
+package com.visualstudioex3.ifcd37weatherforecast
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Entry point for the application and Hilt initializer.
+ */
+@HiltAndroidApp
+class IFCD37WeatherForecastApplication: Application()
