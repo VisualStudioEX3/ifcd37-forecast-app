38b292a017468973997b1dc9c7c21149ad91b0f6
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-03 20:34:05 +0200

    build: Added depedencies
    - Moved Dataframe dependencies to "Adapter" module.
    - Added Hilt dependencies.

--- project/IFCD37WeatherForecast/app/application/build.gradle.kts
+++ project/IFCD37WeatherForecast/app/application/build.gradle.kts
@@ -7,9 +7,6 @@ plugins {
 
     // Secrets Gradle Plugin
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-
-    // Dataframe
-    kotlin("plugin.dataframe") version "2.4.10"
 }
 
 android {
@@ -43,24 +40,6 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
     ksp(libs.hilt.android.compiler)
-
-    // Dataframe
-    implementation(libs.dataframe)
-    implementation(libs.dataframe.core)
-    implementation(libs.dataframe.excel)
-    /*
-     * BUG: Kotlin Dataframe used Slf4j logger under the hood. Seems as bug when program initializes that failed to
-     * find a valid provider and shows annoying log warnings messages on log output.
-     *
-     * Forcing to add Slf4j-NOP provider solve these warnings but still initializes Slf4j as logger (because Kotlin
-     * Dataframe used Kotlin Loggin internally).
-     *
-     * This bug is harmless, only add noise to log output, not compromise the Kotlin Dataframe right execution.
-     *
-     * Possible propper solution seems to be implemented in library side ():
-     * https://youtrack.jetbrains.com/projects/KTNB/issues/KTNB-1198/Log4j-reports-no-logger-dependency-when-dataframe-excel-is-used
-     */
-    implementation(libs.slf4j.nop)
 }
 
 secrets {
