361c75c6c5e5f4025cefb1679ba5c80aa039cc15
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-04 00:50:34 +0200

    feat: Setup service as Hilt injected dependencies

---
+++ project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/ServiceBinder.kt
@@ -0,0 +1,30 @@
+package com.visualstudioex3.application
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Service binder.
+ *
+ * Serve bind functions for resolve dependencies with Hilt using inversion of control.
+ */
+@Suppress("unused")
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ServiceBinder {
+    /**
+     * [MunicipalityFinderImplementation] service binder.
+     *
+     * @param impl The service implementation. It's resolved by Hilt.
+     *
+     * @return Returns a singleton instance of [MunicipalityFinderImplementation] service.
+     */
+    @Binds
+    @Singleton
+    abstract fun bindMunicipalityFinder(
+        impl: MunicipalityFinderImplementation
+    ): MunicipalityFinder
+}
