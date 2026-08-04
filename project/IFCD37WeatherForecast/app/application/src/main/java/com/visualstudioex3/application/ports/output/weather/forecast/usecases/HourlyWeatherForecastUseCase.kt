102dc89be2df4e407569d9c7ea7a271c5b833868
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-04 16:51:52 +0200

    feat: Implemented use case for hourly weather forecast request

---
+++ project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/ports/output/weather/forecast/usecases/HourlyWeatherForecastUseCase.kt
@@ -0,0 +1,11 @@
+package com.visualstudioex3.application.ports.output.weather.forecast.usecases
+
+import com.visualstudioex3.application.UseCase
+import com.visualstudioex3.application.ports.input.weather.forecast.models.HourlyWeatherForecastData
+import com.visualstudioex3.application.ports.input.weather.forecast.models.WeatherForecastRequest
+
+/**
+ * Contract for hourly weather forecast use case.
+ */
+internal interface HourlyWeatherForecastUseCase
+    : UseCase<WeatherForecastRequest, List<HourlyWeatherForecastData>>
