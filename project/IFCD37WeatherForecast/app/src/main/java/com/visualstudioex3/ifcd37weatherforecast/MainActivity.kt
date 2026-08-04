5ab4bac10481c958809cba4618ab85fc2d3927b6
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-04 20:53:59 +0200

    feat: Implemented navigation system in MainActivity.kt
    Used today recomended single activity architecture for different
    screens and views.

--- project/IFCD37WeatherForecast/app/src/main/java/com/visualstudioex3/ifcd37weatherforecast/MainActivity.kt
+++ project/IFCD37WeatherForecast/app/src/main/java/com/visualstudioex3/ifcd37weatherforecast/MainActivity.kt
@@ -4,33 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.visualstudioex3.application.ports.input.MunicipalityFinder
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.visualstudioex3.ifcd37weatherforecast.screens.MunicipalityFinderNavigationTarget
+import com.visualstudioex3.ifcd37weatherforecast.screens.MunicipalityFinderScreen
 import com.visualstudioex3.ifcd37weatherforecast.ui.theme.IFCD37WeatherForecastTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var municipalityFinder: MunicipalityFinder
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             IFCD37WeatherForecastTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Fuenla",
+                    NavigationHandler(
+                        navController = rememberNavController(),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,18 +37,17 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        val municipalities = remember {
-            municipalityFinder.findMunicipalities(name)
-        }
-
-        for (municipality in municipalities) {
-            Row(Modifier.fillMaxWidth()) {
-                Text(
-                    text = "${municipality.name} " +
-                            "(${municipality.province}, ${municipality.autonomousCommunity})",
-                    modifier = Modifier.fillMaxWidth()
-                )
+    fun NavigationHandler(
+        navController: NavHostController,
+        modifier: Modifier
+    ) {
+        NavHost(
+            navController,
+            startDestination = MunicipalityFinderNavigationTarget,
+            modifier = modifier.padding(16.dp)
+        ) {
+            composable<MunicipalityFinderNavigationTarget> {
+                MunicipalityFinderScreen(navController)
             }
         }
     }
