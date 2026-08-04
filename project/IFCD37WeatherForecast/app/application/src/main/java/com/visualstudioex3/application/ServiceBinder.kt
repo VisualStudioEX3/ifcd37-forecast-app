02bedb74d1aa5162a6caaad91c6849b6b0c2d58f
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-04 16:58:10 +0200

    feat: Setup services as Hilt injected dependencies

--- project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/ServiceBinder.kt
+++ project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/ServiceBinder.kt
@@ -2,6 +2,12 @@ package com.visualstudioex3.application
 
 import com.visualstudioex3.application.ports.input.MunicipalityFinder
 import com.visualstudioex3.application.ports.input.MunicipalityFinderImplementation
+import com.visualstudioex3.application.ports.input.weather.forecast.usecases.WeatherForecastUseCase
+import com.visualstudioex3.application.ports.input.weather.forecast.usecases.WeatherForecastUseCaseImplementation
+import com.visualstudioex3.application.ports.output.weather.forecast.usecases.DailyWeatherForecastUseCase
+import com.visualstudioex3.application.ports.output.weather.forecast.usecases.DailyWeatherForecastUseCaseImplementation
+import com.visualstudioex3.application.ports.output.weather.forecast.usecases.HourlyWeatherForecastUseCase
+import com.visualstudioex3.application.ports.output.weather.forecast.usecases.HourlyWeatherForecastUseCaseImplementation
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -29,4 +35,40 @@ abstract class ServiceBinder {
     internal abstract fun bindMunicipalityFinder(
         impl: MunicipalityFinderImplementation
     ): MunicipalityFinder
+
+    /**
+     * [DailyWeatherForecastUseCase] service binder.
+     *
+     * @param impl The service implementation. It's resolved by Hilt.
+     *
+     * @return Returns an instance of [DailyWeatherForecastUseCase] service.
+     */
+    @Binds
+    internal abstract fun bindDailyWeatherForecastUseCase(
+        impl: DailyWeatherForecastUseCaseImplementation
+    ) : DailyWeatherForecastUseCase
+
+    /**
+     * [HourlyWeatherForecastUseCase] service binder.
+     *
+     * @param impl The service implementation. It's resolved by Hilt.
+     *
+     * @return Returns an instance of [HourlyWeatherForecastUseCase] service.
+     */
+    @Binds
+    internal abstract fun bindHourlyWeatherForecastUseCase(
+        impl: HourlyWeatherForecastUseCaseImplementation
+    ) : HourlyWeatherForecastUseCase
+
+    /**
+     * [WeatherForecastUseCase] service binder.
+     *
+     * @param impl The service implementation. It's resolved by Hilt.
+     *
+     * @return Returns an instance of [WeatherForecastUseCase] service.
+     */
+    @Binds
+    internal abstract fun bindWeatherForecastUseCase(
+        impl: WeatherForecastUseCaseImplementation
+    ) : WeatherForecastUseCase
 }
