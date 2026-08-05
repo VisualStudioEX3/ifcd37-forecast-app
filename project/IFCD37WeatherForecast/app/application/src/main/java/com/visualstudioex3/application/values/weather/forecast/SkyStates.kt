201fc8ba58142345e758d8220799ac5335a41398
José Miguel Sánchez Fernández <1664531+VisualStudioEX3@users.noreply.github.com>
2026-08-05 04:22:38 +0200

    feat: Added all missing "night" variants of sky states
    Some test detected missing values with the sufix 'n' on the id field.
    Seems to be more values not documented in AEMET web site. After fixed
    the missing ones in the test, decided to create the all missing 'night'
    variants for the rest of enumeration.

--- project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/values/weather/forecast/SkyStates.kt
+++ project/IFCD37WeatherForecast/app/application/src/main/java/com/visualstudioex3/application/values/weather/forecast/SkyStates.kt
@@ -25,7 +25,9 @@ enum class SkyStates(
     Cloudy("14", 0, 0), // Nuboso
     CloudyNight("14n", 0, 0), // Nuboso noche
     VeryCloudy("15", 0, 0),  // Muy nuboso
+    VeryCloudyNight("15n", 0, 0),  // Muy nuboso (noche, no documentado)
     CoveredSky("16", 0, 0), // Cubierto
+    CoveredSkyNight("16n", 0, 0), // Cubierto (noche, no documentado)
     HightClouds("17", 0, 0), // Nubes altas
     HighCloudsNight("17n", 0, 0), // Nubes altas noche
     PartyCloudyWithLightRain("43", 0, 0), // Intervalos nubosos con lluvia escasa
@@ -33,38 +35,53 @@ enum class SkyStates(
     CloudyWithLightRain("44", 0, 0), // Nuboso con lluvia escasa
     CloudyWithLightRainNight("44n", 0, 0), // Nuboso con lluvia escasa noche
     VeryCloudyWithLightRain("45", 0, 0), // Muy nuboso con lluvia escasa
+    VeryCloudyWithLightRainNight("45n", 0, 0), // Muy nuboso con lluvia escasa (noche, no documentado)
     CoveredWithLightRain("46", 0, 0), // Cubierto con lluvia escasa
+    CoveredWithLightRainNight("46n", 0, 0), // Cubierto con lluvia escasa (noche, no documentado)
     CloudyIntervalsWithRain("23", 0, 0), // Intervalos nubosos con lluvia
     CloudyIntervalsWithRainNight("23n", 0, 0), // Intervalos nubosos con lluvia noche
     CloudyWithRain("24", 0, 0), // Nuboso con lluvia
     CloudyWithRainNight("24n", 0, 0), // Nuboso con lluvia noche
     VeryCloudyWithRain("25", 0, 0), // Muy nuboso con lluvia
+    VeryCloudyWithRainNight("25n", 0, 0), // Muy nuboso con lluvia (noche, no documentado)
     CoveredWithRain("26", 0, 0), // Cubierto con lluvia
+    CoveredWithRainNight("26n", 0, 0), // Cubierto con lluvia (noche, no documentado)
     CloudyIntervalsWithLightSnow("71", 0, 0), // Intervalos nubosos con nieve escasa
     CloudyIntervalsWithLightSnowNight("71n", 0, 0), // Intervalos nubosos con nieve escasa noche
     CloudyWithLightSnow("72", 0, 0), // Nuboso con nieve escasa
     CloudyWithLightSnowNight("72n", 0, 0), // Nuboso con nieve escasa noche
     VeryCloudyWithLightSnow("73", 0, 0), // Muy nuboso con nieve escasa
-    VeryCloudyWithLightSnowNight("74", 0, 0), // Cubierto con nieve escasa
+    VeryCloudyWithLightSnowNight("73n", 0, 0), // Cubierto con nieve escasa (noche, no documentado)
+    CoveredWithLightSnow("74", 0, 0), // Cubierto con nieve escasa
+    CoveredWithLightSnowNight("74n", 0, 0), // Cubierto con nieve escasa (noche, no documentado)
     CloudyIntervalsWithSnow("33", 0, 0), // Intervalos nubosos con nieve
     CloudyIntervalsWithSnowNight("33n", 0, 0), // Intervalos nubosos con nieve noche
     CloudyWithSnow("34", 0, 0), // Nuboso con nieve
     CloudyWithSnowNight("34n", 0, 0), // Nuboso con nieve noche
     VeryCloudyWithSnow("35", 0, 0), // Muy nuboso con nieve
+    VeryCloudyWithSnowNight("35n", 0, 0), // Muy nuboso con nieve (noche, no documentado)
     CoveredWithSnow("36", 0, 0), // Cubierto con nieve
+    CoveredWithSnowNight("36n", 0, 0), // Cubierto con nieve (noche, no documentado)
     ClodyIntervalsWithStorm("51", 0, 0), // Intervalos nubosos con tormenta
     ClodyIntervalsWithStormNight("51n", 0, 0), // Intervalos nubosos con tormenta noche
     CloudyWithStorm("52", 0, 0), // Nuboso con tormenta
     CloudyWithStormNight("52n", 0, 0), // Nuboso con tormenta noche
     VeryCloudyWithStorm("53", 0, 0), // Muy nuboso con tormenta
+    VeryCloudyWithStormNight("53n", 0, 0), // Muy nuboso con tormenta (noche, no documentado)
     CoveredWithStorm("54", 0, 0), // Cubierto con tormenta
+    CoveredWithStormNight("54n", 0, 0), // Cubierto con tormenta (noche, no documentado)
     CloudyIntervalsWithStormAndRain("61", 0, 0), // Intervalos nubosos con tormenta y lluvia escasa
     CloudyIntervalsWithStormAndRainNight("61n", 0, 0), // Intervalos nubosos con tormenta y lluvia escasa noche
     CloudyWithStormAndLightRain("62", 0, 0), // Nuboso con tormenta y lluvia escasa
     CloudyWithStormAndLightRainNight("62n", 0, 0), // Nuboso con tormenta y lluvia escasa noche
     VeryCloudyWithStormAndLightRain("63", 0, 0), // Muy nuboso con tormenta y lluvia escasa
+    VeryCloudyWithStormAndLightRainNight("63n", 0, 0), // Muy nuboso con tormenta y lluvia escasa (noche, no documentado)
     CoveredWithStormAndLightRain("64", 0, 0), // Cubierto con tormenta y lluvia escasa
+    CoveredWithStormAndLightRainNight("64n", 0, 0), // Cubierto con tormenta y lluvia escasa (noche, no documentado)
     Fog("81", 0, 0), // Niebla
+    FogNight("81n", 0, 0), // Niebla (noche, no documentado)
     Mist("82", 0, 0), // Bruma
+    MistNight("82n", 0, 0), // Bruma (noche, no documentado)
     Haze("83", 0, 0), // Calima
+    HazeNight("83n", 0, 0), // Calima (noche, no documentado)
 }
